1ec4a43abac72bd33ef3c4c764f0b1dc5695d088
Karol <kb.appsdesign@gmail.com>
2018-09-11 21:19:44 +0200

    23.3. Exercise finished. Searching elements using XPATH.

--- kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
@@ -4,12 +4,15 @@ import com.kodilla.testing2.config.WebDriverConfig;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 public class FacebookTestingApp {
     public static final String XPATH_WAIT_FOR = "//div[@id=\"reg_box\"]";
     public static final String XPATH_INPUT_FIRSTNAME = "//input[@name=\"firstname\"]";
     public static final String XPATH_INPUT_LASTNAME = "//input[@name=\"lastname\"]";
-
+    public static final String XPATH_SELECT_DAY = "//select[@name=\"birthday_day\"]";
+    public static final String XPATH_SELECT_MONTH = "//select[@name=\"birthday_month\"]";
+    public static final String XPATH_SELECT_YEAR = "//select[@name=\"birthday_year\"]";
 
     public static void main(String[] args) {
         WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
@@ -22,7 +25,20 @@ public class FacebookTestingApp {
 
         WebElement textLastname = driver.findElement(By.xpath(XPATH_INPUT_LASTNAME));
         textLastname.sendKeys("The robotic last name");
-        
+
+        //EXERCISE 23.3 -> going to select 14.02.2018
+        WebElement selectDay = driver.findElement(By.xpath(XPATH_SELECT_DAY));
+        Select selectDayCombo = new Select(selectDay);
+        selectDayCombo.selectByValue("14");
+
+        WebElement selectMonth = driver.findElement(By.xpath(XPATH_SELECT_MONTH));
+        Select selectMonthCombo = new Select(selectMonth);
+        selectMonthCombo.selectByIndex(2);
+
+        WebElement selectYear = driver.findElement(By.xpath(XPATH_SELECT_YEAR));
+        Select selectYearCombo = new Select(selectYear);
+        selectYearCombo.selectByValue("2018");
+
         System.out.println("finished");
     }
 }
